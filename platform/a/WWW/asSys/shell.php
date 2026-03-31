2d873fb1adf30e6aa6661c412561a490354edc04
Dani 'Kat' Leve <the.danibobby@gmail.com>
2026-03-31 02:25:03 -0400

    we have a browser shell and she's DIVINE

--- platform/a/WWW/asSys/shell.php
+++ platform/a/WWW/asSys/shell.php
@@ -2,71 +2,26 @@
 <html><head>
 <title>WWW THE BROWSER WINDOW</title>
 <!-- THE CALLING OF THE STYLESHEET PROCESSION -->
-<style>
-body {
-    height: 98vh;
-    }
-.wwwExplorer_mainShell {
-position: fixed;
-top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-background: gray;
-overflow: hidden; /* Keeps screen content inside */
-}
-
-
-.wwwExplorer_windowTitleBar {
-position: fixed;
-top: 1.5vh;
-left: 1vh;
-right: 1vh;
-height: 3.5vh;
-background-image: linear-gradient(to top, blue, lightblue);
-overflow: hidden; /* Keeps screen content inside */
-box-shadow: 
-    inset -.50vh -.50vh 0 #00000033, 
-    inset .50vh .50vh 0 #FFFFFF66,
-    .1vh .1vh 0 #FFFFFF66,
-    -.1vh -.1vh 0 #00000066;
-border: .15vh solid blue;
-padding: .25vh 1vh;
-}
-
-
-.wwwExplorer_windowToolBar {
-position: fixed;
-top: 6.5vh;
-left: 1vh;
-right: 1vh;
-height: 4vh;
-background: #ffffff33;
-overflow: hidden; /* Keeps screen content inside */
-}
-
-
-.wwwExplorer_innerShell {
-position: fixed;
-top: 12vh;
-left: 1vh;
-right: 1vh;
-bottom: 4vh;
-background: black;
-overflow: scroll; /* Keeps screen content inside */
-}
-
-
-</style>
+<link rel="stylesheet" type="text/css" href="http://localhost:9808/k/incl/fonts.css">';
+<link rel="stylesheet" type="text/css" href="style.css">';
 </head>
 <body>
 
 <div class="wwwExplorer_mainShell"></div>
 <div class="wwwExplorer_windowTitleBar">Concept of Connection "Inter-Network"</div>
-<div class="wwwExplorer_windowToolBar"></div>
+<div class="wwwExplorer_windowToolBar">
+<div class="wwwExplorer_linkBar"><a href="#">< back</a> <a href="#">forward ></a>
+<span class="linkSlug"> >| b://www-old/what-am-i </span> <a href="#">GO!</a></div>
 
+<div class="wwwExplorer_innerShell">
+The World Wide Web (also known as WWW, W3, or simply the Web)[2] is a public interconnected information system that enables content sharing over the Internet.[3] It facilitates access to documents and other web resources according to specific rules of the Hypertext Transfer Protocol (HTTP).[4]
 
-<div class="wwwExplorer_innerShell"></div>
+The Web was invented by English computer scientist Tim Berners-Lee while at CERN in 1989 and opened to the public in 1993. It was conceived as a "universal linked information system".[5][6][7] Documents and other media content are made available to the network through web servers and can be accessed by programs such as web browsers. Servers and resources on the World Wide Web are identified and located through a character string called uniform resource locator (URL).
+
+The original and still very common document type is a web page formatted in Hypertext Markup Language (HTML). This markup language supports plain text, images, embedded video and audio contents, and scripts (short programs) that implement complex user interaction. The HTML language also supports hyperlinks (embedded URLs), which provide immediate access to other web resources. Web navigation, or web surfing, is the common practice of following such hyperlinks across multiple websites. Web applications are web pages that function as application software. The information on the Web is transferred across the Internet using HTTP. Multiple web resources with a common theme and usually a common domain name make up a website. A single web server may provide multiple websites, while some websites, especially the most popular ones, may be provided by multiple servers. Website content is provided by a myriad of companies, organisations, government agencies, and individual users; and comprises an enormous amount of educational, entertainment, commercial, and government information.
+
+The World Wide Web has become the world's dominant information systems platform.[8][9][10][11] It is the primary tool that billions of people worldwide use to interact with the Internet.[4] 
+</div>
 
 
 
