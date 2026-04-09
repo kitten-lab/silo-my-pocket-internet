e259e32813b770e4bd9bfaceb257cfdad13c7768
Dani 'Kat' Leve <the.danibobby@gmail.com>
2026-04-08 22:12:21 -0400

    Rosewood8  D22:T12:N21 on EPO7 GAIA R2026 ED04:ET3:EN08 >| COMMIT 1 >| Leaf:
    Affected files:
    platform/m/rooms/SKYLINE/REGISTRAR/view.php

---
+++ platform/m/rooms/SKYLINE/REGISTRAR/view.php
@@ -0,0 +1,12 @@
+<?php 
+
+openSky('SKYLINE KEYMAKER');
+#getMy_WWW('REGISTRAR\|view');
+
+bigHeading('SKYLINE keyMaker');
+medHeading('A CHESTER\'S IMPORTS PRODUCT');
+getTool("keyMaker","MakeKey");    
+
+closeSky();
+
+ ?>
